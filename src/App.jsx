bfbeddd3fdb1ca8ac2690fd98d4f7a1352334c89
Deepanshu Pandey <deepanshu.06pandey@gmail.com>
2025-08-20 19:11:28 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./Components/layouts/Navbar.jsx";
-import Footer from "./Components/layouts/Footer.jsx";
-import Home from "./Components/layouts/home.jsx";
-import Future from "./Components/layouts/future.jsx";
-import Presence from "./Components/layouts/Presence.jsx";
-import About from "./Components/layouts/About.jsx";
+import Navbar from "./components/layouts/Navbar.jsx";
+import Footer from "./components/layouts/Footer.jsx";
+import Home from "./components/layouts/Home.jsx";
+import Future from "./components/layouts/Future.jsx";
+import Presence from "./components/layouts/Presence.jsx";
+import About from "./components/layouts/About.jsx";
 import Explore from "./pages/Explore.jsx";
 import Services from "./pages/Services.jsx";
 
