9bae73df8a24e676ec69d1e2489c22e9887635d6
Ojaswa kesharwani <ojaswakesharwani@gmail.com>
2025-09-14 03:25:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/PoultryWalaFresh/", // 👈 Add this line (must match repo name)
   plugins: [react(), tailwindcss()],
    server: {
         port: 3000,
